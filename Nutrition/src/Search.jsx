92dff5c7131371441ac259903ce37af1b1ab749c
suhas788 <141901404+suhas788@users.noreply.github.com>
2024-02-28 15:10:45 +0530

    update

---
+++ Nutrition/src/Search.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Search.css"
+
+export default function Search(props){
+    return (
+        <div className="search">
+            <h3>Search</h3>
+            <input>onChange= {e=>{props.handleSearch(e)}} className="input</input>
+    )
+}
